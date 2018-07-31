aee9ce4a476bcdc97ee6119573ad650c175d14ae
mandaliyapraful <mandaliyapraful@gmail.com>
2018-07-31 09:14:02 +0530

    Async file folder and file creation

---
+++ AsycDir_creation.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+fs.mkdir('stuff',function(){
+    fs.readFile('readMe.txt','utf8', function(err, data){
+        fs.writeFile('./stuff/writeMe.txt',data,function(err){
+            if(err) throw err;
+            console.log('File created');
+        });
+    });
+});
